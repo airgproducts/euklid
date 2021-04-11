8ff6e4afaba1a5f1c19e7dee5c5c29f46cec3cd1
hiaselhans <simon.klemenc@gmail.com>
2021-04-11 12:32:35 +0200

    cleanup tests

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,36 +1,9 @@
-#! /usr/bin/python2
-# -*- coding: utf-8; -*-
-#
-# (c) 2013 booya (http://booya.at)
-#
-# This file is part of the OpenGlider project.
-#
-# OpenGlider is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# OpenGlider is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with OpenGlider.  If not, see <http://www.gnu.org/licenses/>.
-import numpy as np
+#! /bin/python
+import unittest
+import random
 
 from euklid.vector import PolyLine3D, PolyLine2D
 import euklid.vector as vec
-
-
-
-#from openglider.vector.functions import norm, normalize, rotation_3d
-#from openglider.vector.polyline import PolyLine, PolyLine2D
-
-
-__author__ = 'simon'
-import unittest
-import random
 
 
 class TestVector3D(unittest.TestCase):
