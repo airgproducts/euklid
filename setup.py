1ce61350dea26be0a36d2a09969f48ce93b10d9e
hiaselhans <simon.klemenc@gmail.com>
2021-07-10 07:52:12 +0200

    add toml file

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ class CMakeBuild(build_ext):
         if not os.path.exists(self.build_lib):
             stubgen_path = self.build_temp
 
-        stubgen_exec = subprocess.check_call([sys.executable, 'stubs.py', stubgen_path])
+        subprocess.check_call([sys.executable, 'stubs.py', stubgen_path])
 
         
 
